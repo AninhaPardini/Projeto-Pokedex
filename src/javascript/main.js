87bd072d2fab61389ef969934e233d7b08b78907
AninhaPardini <aninhapardini.work@gmail.com>
2023-09-03 14:46:54 -0300

    🔍 Adicionando o debounce e tentando blur

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -8,7 +8,7 @@ let limit = 10;
 
 function loadPokemons(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map((pokemon) => 
+        const newHtml = pokemons.map((pokemon) =>
             `
             <li class="pokemon ${pokemon.type}">
                 <span class="number">#${pokemon.number}</span>
@@ -46,32 +46,42 @@ const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
 limit = 151;
 
-searchButton.addEventListener('click', async (offset, limit) => {
-    const search = searchInput.value.toLowerCase();
+const debounce = (callback, wait) => { // função para esperar um tempo antes de executar a função
+    let timeoutId = null;
+    return (...args) => {
+        window.clearTimeout(timeoutId);
+        timeoutId = window.setTimeout(() => {
+            callback.apply(null, args);
+        }, wait);
+    };
+}
 
-    const pokemons = await pokeApi.getSearchPokemons(search);
-    const newHtml = pokemons.map((pokemon) =>
+searchInput.addEventListener('keyup', debounce(async (offset, limit) => {
+    const search = searchInput.value.toLowerCase();
+    console.log('change', searchInput.value);
+    const pokemonsAll = await pokeApi.getSearchPokemons(search);
+    const newHtml = pokemonsAll.map((pokemon) =>
         `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
         
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
             
-                <img src="${pokemon.image}"
-                    alt="${pokemon.name}">
-            </div>
-        </li>
-        `
+                    <img src="${pokemon.image}"
+                        alt="${pokemon.name}">
+                </div>
+            </li>
+            `
     ).join('')
     pokemonsElement.innerHTML = newHtml
-    
+
     const removeLoadMoreButton = document.getElementById('loadMoreButton');
     removeLoadMoreButton.classList.add('hidden');
-});
+}, 100));
 
 
 
